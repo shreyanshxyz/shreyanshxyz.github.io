c797996b5754e2c551a3e2fdf1372e79faebad5f
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-02 23:21:07 +0530

    delete 1 file and update 3 files

--- src/components/footer/FooterText.js
+++ src/components/footer/FooterText.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Link } from '@material-ui/core';
-import { TextDecrypt } from '../content/TextDecrypt';
-import { HeartIcon } from '../content/SponsorButton';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Link } from "@material-ui/core";
+import { TextDecrypt } from "../content/TextDecrypt";
+import { HeartIcon } from "../content/SponsorButton";
 
 const useStyles = makeStyles((theme) => ({
   footerText: {
-    position: 'absolute',
+    position: "absolute",
     bottom: theme.spacing(6),
     left: theme.spacing(6),
-    '&:hover': {
+    "&:hover": {
       color: theme.palette.primary.main,
     },
-    transition: 'all 0.5s ease',
-    display: 'flex',
-    alignItems: 'center',
-    flexWrap: 'wrap',
+    transition: "all 0.5s ease",
+    display: "flex",
+    alignItems: "center",
+    flexWrap: "wrap",
   },
 }));
 
@@ -24,16 +24,16 @@ export const FooterText = () => {
 
   return (
     <Link
-      color='inherit'
-      underline='none'
-      href='https://github.com/sponsors/JoHoop'
-      target='_blank'
-      rel='noopener noreferrer'
+      color="inherit"
+      underline="none"
+      href="https://github.com/sponsors/JoHoop"
+      target="_blank"
+      rel="noopener noreferrer"
       className={classes.footerText}
     >
       <HeartIcon />
-      <Typography variant='body1'>
-        <TextDecrypt text={' Sponsor'} />
+      <Typography variant="body1">
+        <TextDecrypt text={"->  Resume"} />
       </Typography>
     </Link>
   );
